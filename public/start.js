792400b28953d0ed47142b7a8b74106e1b0d291c
xiavanessa <vanessa.xcf@gmail.com>
2026-02-06 09:20:25 +0100

    initial commit

--- public/start.js
+++ public/start.js
@@ -1,1 +1,35 @@
 console.log("Hello from start.js");
+//1.
+const source =
+  "https://gist.githubusercontent.com/mapoto/515827c73f7f60a14cf2914f0cca16f0/raw/c41bc850bc989cadf6a1caa394ab12fce105d59e/countries.json";
+
+const body = document.querySelector("body");
+let countryList = document.createElement("ul");
+body.appendChild(countryList);
+
+fetch(source)
+  .then((response) => response.json())
+  .then((data) => {
+    data.map((country) => {
+      console.log(country.name);
+      let countryEle = document.createElement("li");
+      countryEle.textContent = country.name;
+      countryList.appendChild(countryEle);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+//2.
+const imgUrl = "https://avatars.githubusercontent.com/u/38219434?v=4";
+
+fetch(imgUrl)
+  .then((response) => response.blob())
+  .then((blob) => {
+    let imgElement = document.createElement("img");
+    let imgURL = URL.createObjectURL(blob);
+    imgElement.src = imgURL;
+    document.body.appendChild(imgElement);
+  })
+  .catch((error) => console.error("Error fetching image:", error));
